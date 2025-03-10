ab6b69b1756c54005ce2343d2c060670e3314776
swepradeep <sweswepradeep@gmail.com>
2025-03-10 12:16:51 +0530

    transition smooth of volume ripple

--- src/components/VolumeRipple.tsx
+++ src/components/VolumeRipple.tsx
@@ -4,10 +4,15 @@ const VolumeRipple: React.FC<any> = ({ volumeLevel, children }: any) => {
   return (
     <div className={`w-full h-full flex items-center justify-center`}>
       <div
-        className={`w-fit h-fit rounded-full bg-[#000000]/30`}
-        style={{ padding: `${volumeLevel * 50}px ${volumeLevel * 50}px` }}
+        className={`w-fit h-fit rounded-full border duration-300`}
+        style={{ padding: `${volumeLevel * 60}px ${volumeLevel * 60}px` }}
       >
-        {children}
+        <div
+          className={`w-fit h-fit rounded-full bg-[#1d1d1d]/30 duration-300`}
+          style={{ padding: `${volumeLevel * 50}px ${volumeLevel * 50}px` }}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
