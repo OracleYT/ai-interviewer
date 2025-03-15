2743b9148ce53ccefa78a8d3dd85fbab1377039d
swepradeep <sweswepradeep@gmail.com>
2025-03-15 14:14:55 +0530

    video on starting

--- src/components/ParticipantItem.tsx
+++ src/components/ParticipantItem.tsx
@@ -1,16 +1,26 @@
 import useUserColor from "@/hooks/useUserColor";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import VolumeRipple from "./VolumeRipple";
 import { VolumeLevelContext } from "@/contexts/VolumeLevelProvider";
 import { BrowserMediaContext } from "@/contexts/BrowserMediaProvider";
 import clsx from "clsx";
+import MicOff from "./icons/MicOff";
+import Mic from "./icons/Mic";
 
 export const ParticipantItem = ({ participant }: any) => {
   const firstNameLetter = participant.name.split(" ")[0][0]?.toUpperCase();
   const color = useUserColor()(participant.name);
   const { volumeLevel } = useContext(VolumeLevelContext);
   const { videoRef, isCameraOn } = useContext(BrowserMediaContext);
+  const [showVideo, setShowVideo] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowVideo(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center border rounded-lg bg-[#303030] w-full select-none h-100">
       <div className="relative w-full h-full flex items-center justify-center rounded-lg overflow-hidden bg-gray-300">
@@ -22,14 +32,27 @@ export const ParticipantItem = ({ participant }: any) => {
             muted
             style={{ objectFit: "cover" }}
           />
-        ) : participant.image ? (
-          <VolumeRipple volumeLevel={volumeLevel}>
-            <img
-              src={participant.image}
-              alt={participant.name}
-              className={`w-32 h-32 rounded-full object-cover`}
+        ) : showVideo ? (
+          <div className="relative w-full h-full">
+            <video
+              className={`w-full h-full object-cover`}
+              src="/7581332-hd_1920_1080_30fps.mp4"
+              autoPlay
+              muted
             />
-          </VolumeRipple>
+            {/* <MicOff  className="absolute top-5 right-5 h-10 w-10 rounded-full bg-[#484848]/30 flex justify-center items-center"/> */}
+          </div>
+        ) : participant.image ? (
+          <div className="relative w-full h-full">
+            <VolumeRipple volumeLevel={volumeLevel}>
+              <img
+                src={participant.image}
+                alt={participant.name}
+                className={`w-32 h-32 rounded-full object-cover`}
+              />
+            </VolumeRipple>
+            {/* <Mic className="absolute top-5 right-5 h-10 w-10 rounded-full bg-[#484848]/30 flex justify-center items-center"/> */}
+          </div>
         ) : (
           <div
             className={clsx(
